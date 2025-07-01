f7cafe83f267aa0b8a4dbd4c13bd379abaf78769
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-07-01 18:10:25 -0300

    refactor: use sets to compute differences in components
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- src/channel.rs
+++ src/channel.rs
@@ -1,4 +1,5 @@
-use std::{borrow::Cow, fmt};
+use std::hash::{Hash, Hasher};
+use std::{borrow::Cow, collections::HashSet, fmt};
 
 use serde::{Deserialize, Serialize};
 
@@ -42,24 +43,59 @@ impl Channel {
     }
 
     pub fn components_to_update(&self, newer: &Self) -> Vec<Component> {
-        // NOTE: This wouldn't work if they have different amount of elements
+        let new: HashSet<&Component> = HashSet::from_iter(newer.components.iter());
+        let current: HashSet<&Component> = HashSet::from_iter(self.components.iter());
 
-        let mut updates = Vec::new();
-        for new_component in newer.components.iter() {
-            let old_component = self.components.iter().find(|c| c.name == new_component.name);
-            if let Some(old_component) = old_component {
-                if old_component != new_component {
-                    updates.push(new_component.clone());
-                }
+        // This is the subset of new components present in the channel since
+        // last sync.
+        let new_components = new.difference(&current);
+
+        // This is the subset of old components that need to be removed.
+        let old_components = current.difference(&new);
+
+        // These are the elements that are present in boths sets. We need which
+        // components need updating.
+        // NOTE: Equality between components is done via their name, see
+        // [Component::eq].
+        let components_to_update = current.intersection(&new).filter(|current_component| {
+            let new_component = new.get(*current_component);
+            // We are only interested in component which are different
+            if let Some(new_component) = new_component {
+                !current_component.is_the_same(new_component)
             } else {
-                // If the new component does not exist in the old component
-                // list, then that means it was added in an update and must be
-                // installed
-                updates.push(new_component.clone());
-            };
-        }
+                // This should't be possible, but if somehow the component is
+                // missing, then we trigger an update for said component
+                // regardless.
+                true
+            }
+        });
 
-        updates
+        let components = new_components
+            .chain(old_components)
+            .chain(components_to_update)
+            .map(|c| (*c).clone());
+
+        Vec::from_iter(components)
+    }
+}
+
+impl Eq for Component {}
+impl PartialEq for Component {
+    fn eq(&self, other: &Self) -> bool {
+        // NOTE: Two component are "partially equal" if their names are the
+        // same. This does not mean that they're equal, since they could differ
+        // in fields like versions.
+        // This is implmented manually, in order to make use of HashSets with
+        // components.
+        self.name == other.name
+    }
+}
+impl Hash for Component {
+    fn hash<H>(&self, state: &mut H)
+    where
+        H: Hasher,
+    {
+        self.name.hash(state)
     }
 }
 
@@ -132,7 +168,7 @@ impl core::str::FromStr for ChannelAlias {
 }
 
 /// An installable component of a toolchain
-#[derive(Serialize, Deserialize, Debug, Clone, Hash, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Component {
     /// The canonical name of this toolchain component
     pub name: Cow<'static, str>,
@@ -168,6 +204,25 @@ impl Component {
             requires: vec![],
             rustup_channel: None,
             installed_file: None,
+        }
+    }
+
+    /// NOTE: This method is used to check if two components share properties
+    /// BESIDES the name. The [Component::eq] implementation (which only tests
+    /// name equality) is used to comply with the std's requirements.
+    pub fn is_the_same(&self, other: &Self) -> bool {
+        if self.version != other.version {
+            return false;
+        } else if self.features != other.features {
+            return false;
+        } else if self.requires != other.requires {
+            return false;
+        } else if self.rustup_channel != other.rustup_channel {
+            return false;
+        } else if self.installed_file != other.installed_file {
+            return false;
+        } else {
+            true
         }
     }
 }
@@ -225,5 +280,141 @@ impl core::str::FromStr for UserChannel {
                 .map(Self::Version)
                 .map_err(|err| anyhow!("invalid channel version: {err}")),
         }
+    }
+}
+
+mod tests {
+    use crate::channel::Channel;
+    use crate::channel::Component;
+    use crate::version::Authority;
+
+    #[test]
+    fn check_components_to_update() {
+        let old_components = [
+            Component {
+                name: std::borrow::Cow::Borrowed("vm"),
+                version: Authority::Cargo {
+                    package: Some(String::from("miden-vm")),
+                    version: semver::Version::new(0, 12, 0),
+                },
+                features: vec![String::from("executable"), String::from("concurrent")],
+                requires: Vec::new(),
+                rustup_channel: None,
+                installed_file: None,
+            },
+            Component {
+                name: std::borrow::Cow::Borrowed("std"),
+                version: Authority::Cargo {
+                    package: Some(String::from("miden-stdlib")),
+                    version: semver::Version::new(0, 15, 0),
+                },
+                features: Vec::new(),
+                requires: Vec::new(),
+                rustup_channel: None,
+                installed_file: None,
+            },
+            Component {
+                name: std::borrow::Cow::Borrowed("removed-component"),
+                // This should be present in the update vector
+                // Component that got removed
+                version: Authority::Cargo {
+                    package: Some(String::from("deleted-repo")),
+                    version: semver::Version::new(0, 82, 77),
+                },
+                features: Vec::new(),
+                requires: Vec::new(),
+                rustup_channel: None,
+                installed_file: None,
+            },
+            Component {
+                name: std::borrow::Cow::Borrowed("base"),
+                version: Authority::Cargo {
+                    package: Some(String::from("miden-lib")),
+                    version: semver::Version::new(0, 9, 0),
+                },
+                features: Vec::new(),
+                requires: Vec::new(),
+                rustup_channel: None,
+                installed_file: None,
+            },
+        ];
+
+        let new_components = [
+            Component {
+                name: std::borrow::Cow::Borrowed("vm"),
+                // This should be present in the update vector
+                // Newer version
+                version: Authority::Cargo {
+                    package: Some(String::from("miden-vm")),
+                    version: semver::Version::new(0, 15, 0),
+                },
+                features: vec![String::from("executable"), String::from("concurrent")],
+                requires: Vec::new(),
+                rustup_channel: None,
+                installed_file: None,
+            },
+            Component {
+                name: std::borrow::Cow::Borrowed("std"),
+                // This should be present in the update vector
+                // Newer version
+                version: Authority::Cargo {
+                    package: Some(String::from("miden-stdlib")),
+                    version: semver::Version::new(0, 16, 0),
+                },
+                features: Vec::new(),
+                requires: Vec::new(),
+                rustup_channel: None,
+                installed_file: None,
+            },
+            Component {
+                name: std::borrow::Cow::Borrowed("new-component"),
+                // This should be present in the update vector
+                // New component all together.
+                version: Authority::Cargo {
+                    package: Some(String::from("new-repo")),
+                    version: semver::Version::new(78, 69, 87),
+                },
+                features: Vec::new(),
+                requires: Vec::new(),
+                rustup_channel: None,
+                installed_file: None,
+            },
+            Component {
+                name: std::borrow::Cow::Borrowed("base"),
+                version: Authority::Cargo {
+                    package: Some(String::from("miden-lib")),
+                    version: semver::Version::new(0, 9, 0),
+                },
+                features: Vec::new(),
+                requires: Vec::new(),
+                rustup_channel: None,
+                installed_file: None,
+            },
+        ];
+
+        let old = Channel {
+            name: semver::Version::new(0, 0, 1),
+            alias: None,
+            components: old_components.to_vec(),
+        };
+
+        let new = Channel {
+            name: semver::Version::new(0, 0, 2),
+            alias: None,
+            components: new_components.to_vec(),
+        };
+
+        let components = old.components_to_update(&new);
+
+        // To see how many elements and a brief explanation regarding why, run
+        // the following command from the project root:
+        // grep "This should be present in the update vector" src/channel.rs -A 1 -B 1
+        // Sidenote: This line will also appear
+
+        assert_eq!(components.len(), 4);
+        assert!(components.iter().any(|c| c.name == "vm"));
+        assert!(components.iter().any(|c| c.name == "removed-component"));
+        assert!(components.iter().any(|c| c.name == "std"));
+        assert!(components.iter().any(|c| c.name == "new-component"));
     }
 }
