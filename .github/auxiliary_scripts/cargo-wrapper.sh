69d486cc444ba673ea8a28ab02c21e452c1e7664
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-07-17 09:49:20 -0300

    feat: generate cargo-wrapper script on demand
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- .github/auxiliary_scripts/cargo-wrapper.sh
+++
@@ -1,9 +0,0 @@
-cargo() {
-    if [[ $@ == "+nightly" ]]; then
-        command /home/runner/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin/cargo
-    elif [[ $@ == "+nightly-2025-03-20" ]]; then
-        command /home/runner/.rustup/toolchains/nightly-2025-03-20-x86_64-unknown-linux-gnu/bin/cargo
-    else
-        command /home/runner/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/cargo
-    fi
-}
