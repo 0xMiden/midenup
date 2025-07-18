4e092a1648cf893c5b002e4411935b11e65b15a7
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-07-18 16:12:58 -0300

    feat: add a --debug flag to install debug builds. Used to speed tests up
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -47,7 +47,7 @@ pub fn install(
         format!("failed to create file for install script at '{}'", install_file_path.display())
     })?;
 
-    let install_script_contents = generate_install_script(channel);
+    let install_script_contents = generate_install_script(config, channel);
     install_file.write_all(&install_script_contents.into_bytes()).with_context(|| {
         format!("failed to write install script at '{}'", install_file_path.display())
     })?;
@@ -153,7 +153,7 @@ pub fn install(
 /// This function generates the install script that will later be saved in
 /// `midenup/toolchains/<version>/install.rs`. This file is then executed by
 /// `cargo -Zscript`.
-fn generate_install_script(channel: &Channel) -> String {
+fn generate_install_script(config: &Config, channel: &Channel) -> String {
     // Prepare install script template
     let engine = upon::Engine::new();
     let template = engine
@@ -236,6 +236,7 @@ fn main() {
             "{{ component.required_toolchain_flag }}",
             )
             .arg("install")
+            .arg("{{ debug_mode }}")
             .args([
             {%- for arg in component.args %}
             "{{ arg }}",
@@ -371,6 +372,12 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
+    let debug_mode = if config.debug {
+        "profile --debug"
+    } else {
+        "profile --release"
+    };
+
     // Render the install script
     template
         .render(
@@ -379,29 +386,9 @@ fn main() {
                 dependencies: dependencies,
                 installable_components: installable_components,
                 channel_json : serde_json::to_string_pretty(channel).unwrap(),
+                debug_mode: debug_mode,
             },
         )
         .to_string()
         .unwrap_or_else(|err| panic!("install script rendering failed: {err}"))
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::{UserChannel, manifest::Manifest};
-
-    #[test]
-    fn install_script_template_from_local_manifest() {
-        let manifest = Manifest::load_from("file://manifest/channel-manifest.json").unwrap();
-
-        let channel = manifest
-            .get_channel(&UserChannel::Stable)
-            .expect("Could not convert UserChannel to internal channel representation");
-
-        let script = generate_install_script(channel);
-
-        println!("{script}");
-
-        assert!(script.contains("// Install cargo-miden"));
-    }
 }
