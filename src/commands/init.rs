05b35373ec28f6ec8caf18b37d12aabadd2b8b7f
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-07-22 11:02:22 -0300

    refactor: init no longer installs stable
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,27 +1,18 @@
-use anyhow::Context;
-
-use crate::{commands, config::ensure_midenup_home_exists, manifest::Manifest, Config};
+use crate::{config::ensure_midenup_home_exists, manifest::Manifest, Config};
 
 /// This is the first command the user runs after first installing the midenup. It performs the
 /// following tasks:
 ///
 /// - Bootstrap the `midenup` environment (create directories, default config, etc.), if not already
 ///   done. (done in [ensure_midenup_home_exists]).
-/// - Install the stable channel.
-pub fn init(config: &Config, local_manifest: &mut Manifest) -> anyhow::Result<()> {
+pub fn init(config: &Config, _local_manifest: &mut Manifest) -> anyhow::Result<()> {
     ensure_midenup_home_exists(config)?;
 
-    let toolchains_dir = config.midenup_home.join("toolchains");
-    let stable = toolchains_dir.join("stable");
-    if !stable.exists() {
-        std::println!("About to install stable toolchain");
-
-        let upstream_stable = config
-            .manifest
-            .get_latest_stable()
-            .context("ERROR: No stable channel found in upstream")?;
-        commands::install(config, upstream_stable, local_manifest)?;
-    }
+    println!(
+        "midenup was successfully initialized in:
+{}",
+        config.midenup_home.as_path().display()
+    );
 
     Ok(())
 }
