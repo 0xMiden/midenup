c86146a6814928f8dd2e634a5fb5af4a5adeb5d9
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-08-01 10:37:53 -0300

    fix: replace the call to `miden --version` with a call to `miden help`
    The miden command does not recognize the --version flag; replac
    
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -78,7 +78,7 @@ pub fn setup_midenup(config: &Config) -> anyhow::Result<bool> {
         .stdout(std::process::Stdio::null())
         .stderr(std::process::Stdio::null())
         .stdin(std::process::Stdio::null())
-        .arg("--version")
+        .arg("help")
         .output()
         .is_ok();
 
