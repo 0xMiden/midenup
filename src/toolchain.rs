f186bcd333acdf220af96a48abb06a96b64bce2f
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-08-01 12:18:44 -0300

    feat: add ToolchainJustification enum to improve messages
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- src/toolchain.rs
+++ src/toolchain.rs
@@ -45,6 +45,17 @@ impl Default for Toolchain {
     }
 }
 
+/// Used to specify why Midenup believes the current toolchain is what it is.
+pub enum ToolchainJustification {
+    /// There exists a miden toolchain file present in
+    /// [[MidenToolchainFile::path]].
+    MidenToolchainFile { path: PathBuf },
+    /// The system's default toolchain was overriden (via `miden set`).
+    Override,
+    /// No toolchain was specified, fallback to stable.
+    Default,
+}
+
 impl Toolchain {
     pub fn new(channel: UserChannel, components: Vec<String>) -> Self {
         Toolchain { channel, components }
@@ -63,7 +74,7 @@ impl Toolchain {
     ///    added to the `midenup` directory.
     ///
     /// If none of the previous conditions are met, then `stable` will be used.
-    pub fn current(config: &Config) -> anyhow::Result<Self> {
+    pub fn current(config: &Config) -> anyhow::Result<(Toolchain, ToolchainJustification)> {
         let local_toolchain = Self::toolchain_file()?;
         let global_toolchain = config.midenup_home.join("toolchains").join("default");
 
@@ -78,7 +89,10 @@ impl Toolchain {
 
             let current_toolchain = toolchain_file.inner_toolchain();
 
-            Ok(current_toolchain)
+            Ok((
+                current_toolchain,
+                ToolchainJustification::MidenToolchainFile { path: local_toolchain },
+            ))
         } else if std::fs::read_link(&global_toolchain).is_ok() {
             let channel_path = std::fs::read_link(&global_toolchain).context(format!(
                 "Couldn't read 'default' symlink. Is {} a symlink?",
@@ -113,9 +127,9 @@ impl Toolchain {
             };
             let toolchain = Toolchain { channel, components };
 
-            Ok(toolchain)
+            Ok((toolchain, ToolchainJustification::Override))
         } else {
-            Ok(Self::default())
+            Ok((Toolchain::default(), ToolchainJustification::Default))
         }
     }
 
@@ -123,7 +137,7 @@ impl Toolchain {
         config: &Config,
         local_manifest: &mut Manifest,
     ) -> anyhow::Result<Self> {
-        let current_toolchain = Self::current(config)?;
+        let (current_toolchain, _justification) = Toolchain::current(config)?;
         let desired_channel = &current_toolchain.channel;
 
         let Some(channel) = config.manifest.get_channel(desired_channel) else {
