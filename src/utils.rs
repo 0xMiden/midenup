90b46dad472daf9cf20cc1409a63303d2b90b3d8
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-09-17 18:03:54 -0300

    feat: implement latest_modification_function to get latest modified file
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use std::{fs, path::PathBuf, time::SystemTime};
+
 /// This file contains some general purpose functions.
 use anyhow::Context;
 
@@ -37,3 +39,60 @@ pub fn find_latest_hash(repository_url: &str, branch_name: &str) -> anyhow::Resu
 
     Ok(revision_hash)
 }
+
+/// Returns the latest registered modification time inside a directory,
+/// including its subdirectories. This is intended as a "best effort"
+/// aproximation, if it encounters any errors while reading an entry, it simply
+/// skips it. Additionally, as a safety net, the [[ENTRY_LIMIT]] sets an upper
+/// bound on the number of entries the function can check before returning.
+const ENTRY_LIMIT: u32 = u32::MAX;
+pub fn latest_modification(dir: PathBuf) -> SystemTime {
+    fn traverse_directories(
+        dir: PathBuf,
+        latest: SystemTime,
+        current_entry: u32,
+    ) -> (SystemTime, u32) {
+        let mut local_latest = latest;
+        let mut current_entry_count = current_entry;
+
+        let entries = fs::read_dir(&dir);
+        if let Ok(entries) = entries {
+            for file in entries {
+                let Ok(file) = file else {
+                    continue;
+                };
+                let Ok(metadata) = file.metadata() else {
+                    continue;
+                };
+
+                if current_entry_count == ENTRY_LIMIT {
+                    break;
+                }
+
+                let (current_entry_latest, visited_entries) =
+                    // We avoid symlinks to directories to avoid infinite loops.
+                    if metadata.is_dir() && !metadata.is_symlink() {
+                        traverse_directories(file.path(), local_latest, current_entry_count)
+                    } else {
+                        (metadata.modified().unwrap_or(local_latest), current_entry_count + 1)
+                    };
+                current_entry_count = visited_entries;
+
+                if current_entry_latest > local_latest {
+                    local_latest = current_entry_latest
+                }
+            }
+        } else {
+            println!("Failed to open {}, skipping it.", dir.display());
+        }
+
+        (local_latest, current_entry_count)
+    }
+
+    let directory_last_modification = dir.metadata().unwrap().modified().unwrap();
+
+    let (latest_found_modification, _) = traverse_directories(dir, directory_last_modification, 0);
+
+    latest_found_modification
+}
+
