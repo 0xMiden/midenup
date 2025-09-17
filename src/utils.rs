f73d6d3d1696afd0db4b4fc173a2eca1bd659870
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-09-17 18:41:34 -0300

    refactor: remove unwraps from latest_modification
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::PathBuf, time::SystemTime};
+use std::{cmp::max, fs, path::PathBuf, time::SystemTime};
 
 /// This file contains some general purpose functions.
 use anyhow::Context;
@@ -46,12 +46,12 @@ pub fn find_latest_hash(repository_url: &str, branch_name: &str) -> anyhow::Resu
 /// skips it. Additionally, as a safety net, the [[ENTRY_LIMIT]] sets an upper
 /// bound on the number of entries the function can check before returning.
 const ENTRY_LIMIT: u32 = u32::MAX;
-pub fn latest_modification(dir: PathBuf) -> SystemTime {
+pub fn latest_modification(dir: PathBuf) -> anyhow::Result<SystemTime> {
     fn traverse_directories(
         dir: PathBuf,
-        latest: SystemTime,
+        latest: Option<SystemTime>,
         current_entry: u32,
-    ) -> (SystemTime, u32) {
+    ) -> (Option<SystemTime>, u32) {
         let mut local_latest = latest;
         let mut current_entry_count = current_entry;
 
@@ -74,13 +74,19 @@ pub fn latest_modification(dir: PathBuf) -> SystemTime {
                     if metadata.is_dir() && !metadata.is_symlink() {
                         traverse_directories(file.path(), local_latest, current_entry_count)
                     } else {
-                        (metadata.modified().unwrap_or(local_latest), current_entry_count + 1)
+                        (metadata.modified().ok(), current_entry_count + 1)
                     };
+
                 current_entry_count = visited_entries;
 
-                if current_entry_latest > local_latest {
-                    local_latest = current_entry_latest
-                }
+                local_latest = match (local_latest, current_entry_latest) {
+                    (Some(local_latest), Some(current_entry_latest)) => {
+                        Some(max(current_entry_latest, local_latest))
+                    },
+                    (Some(local_latest), None) => Some(local_latest),
+                    (None, Some(current_entry_latest)) => Some(current_entry_latest),
+                    (None, None) => None,
+                };
             }
         } else {
             println!("Failed to open {}, skipping it.", dir.display());
@@ -89,10 +95,27 @@ pub fn latest_modification(dir: PathBuf) -> SystemTime {
         (local_latest, current_entry_count)
     }
 
-    let directory_last_modification = dir.metadata().unwrap().modified().unwrap();
+    let directory_last_modification = dir.metadata().and_then(|file| file.modified()).ok();
 
     let (latest_found_modification, _) = traverse_directories(dir, directory_last_modification, 0);
 
-    latest_found_modification
+    // This should only be an error if every single metadata read failed, which should be unlikely.
+    latest_found_modification.context("Failed to read any file")
 }
 
+#[cfg(test)]
+mod tests {
+    use chrono::{DateTime, Local};
+
+    use super::latest_modification;
+
+    #[test]
+    fn latest_modification_test() {
+        let result =
+            latest_modification("/Users/fabri/Repositories/miden-compiler-lambda-fork".into());
+        let date: DateTime<Local> = result.unwrap().clone().into();
+
+        // std::dbg!(result.as_ref());
+        std::dbg!(date);
+    }
+}
