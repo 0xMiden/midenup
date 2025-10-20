310bc8f84d8c2c18d9d5565676ce18c6021841d1
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-10-20 19:40:41 -0300

    feat: add etc directory in miden toolchain
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -340,6 +340,12 @@ fn main() {
     // installation finished successfully.
     let checkpoint_path = miden_sysroot_dir.join("installation-successful");
     rename(progress_path, checkpoint_path).expect("Couldn't rename .installation-in-progress to installation-successful");
+
+    // Create etc directory
+    let etc_dir = miden_sysroot_dir.join("etc");
+    if !std::fs::exists(&etc_dir).unwrap_or(false) {
+        std::fs::create_dir(&etc_dir).expect("Failed to create etc directory toolchain directory.");
+    }
 }
 "##,
         )
