49f31eeb9d65d030c5266b62baead97edb69ae5c
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2026-03-17 09:33:30 -0300

    feat: build Releases struct before iterating
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

--- scripts/update-manifest/src/main.rs
+++ scripts/update-manifest/src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use midenup::channel::semver;
 use midenup::channel::Channel;
+use midenup::channel::Component;
 use midenup::manifest::Manifest;
 use midenup::version::Authority;
 use std::collections::HashMap;
@@ -50,41 +51,71 @@ fn get_vm_version(channel: &Channel) -> Option<&semver::Version> {
 }
 
 type CrateName = String;
+type CrateVersion = semver::Version;
+type MidenVMVersion = semver::Version;
 
+#[derive(Debug)]
 struct Releases {
-    releases: HashMap<CrateName, Vec<semver::Version>>,
+    releases: HashMap<CrateName, HashMap<CrateVersion, MidenVMVersion>>,
 }
 
 impl Releases {
-    fn new() -> Self {
-        Self { releases: HashMap::new() }
+    // TODO: Save in disk already known releases with their corresponding VM versions.
+    // Only fetch the new ones.
+    // We iterate over the Manifest twice since creating the releases struct
+    // consists of a lot of paralellizable IO operations.
+    fn new(manifest: &Manifest) -> Self {
+        let mut releases: HashMap<CrateName, HashMap<CrateVersion, MidenVMVersion>> =
+            HashMap::new();
+        let placeholder = semver::Version::new(0, 0, 0);
+        for channel in manifest.get_channels() {
+            for component in &channel.components {
+                let Authority::Cargo { package, .. } = &component.version else {
+                    continue;
+                };
+                let crate_name = package.as_deref().unwrap_or(&component.name);
+                if releases.contains_key(crate_name) {
+                    continue;
+                }
+                let versions = find_all_versions(crate_name)
+                    .unwrap_or_else(|e| panic!("Could not query crates.io for {crate_name}: {e}"));
+                let version_map: HashMap<CrateVersion, MidenVMVersion> =
+                    versions.into_iter().map(|v| (v, placeholder.clone())).collect();
+                releases.insert(crate_name.to_string(), version_map);
+            }
+        }
+        Self { releases }
     }
 
-    fn get(&mut self, crate_name: &str) -> anyhow::Result<&Vec<semver::Version>> {
-        if !self.releases.contains_key(crate_name) {
-            let versions = find_all_versions(crate_name)?;
-            self.releases.insert(crate_name.to_string(), versions);
-        }
-        Ok(self.releases.get(crate_name).unwrap())
-    }
+    // fn get(&mut self, crate_name: &str) -> anyhow::Result<&Vec<semver::Version>> {
+    //     if !self.releases.contains_key(crate_name) {
+    //         let versions = find_all_versions(crate_name)?;
+    //         self.releases.insert(crate_name.to_string(), versions);
+    //     }
+    //     Ok(self.releases.get(crate_name).unwrap())
+    // }
+}
+
+fn update_component(component: &Component, versions: &[semver::Version]) -> Component {
+    todo!()
 }
 
 fn update_channel(channel: &Channel, releases: &mut Releases, options: &Options) -> Channel {
     let vm_version = get_vm_version(channel).expect("Could not find VM version in channel");
     println!("    VM version: {vm_version}");
 
-    for component in &channel.components {
-        let Authority::Cargo { package, .. } = &component.version else {
-            continue;
-        };
-        let crate_name = package.as_deref().unwrap_or(&component.name);
-        let versions = releases
-            .get(crate_name)
-            .unwrap_or_else(|e| panic!("Could not query crates.io for {crate_name}: {e}"));
-        for version in versions {
-            println!("    {crate_name} {version}");
-        }
-    }
+    // for component in &channel.components {
+    //     let Authority::Cargo { package, .. } = &component.version else {
+    //         continue;
+    //     };
+    //     let crate_name = package.as_deref().unwrap_or(&component.name);
+    //     let versions = releases
+    //         .get(crate_name)
+    //         .unwrap_or_else(|e| panic!("Could not query crates.io for {crate_name}: {e}"));
+    //     for version in versions {
+    //         println!("    {crate_name} {version}");
+    //     }
+    // }
 
     channel.clone()
 }
@@ -98,7 +129,8 @@ fn main() -> anyhow::Result<()> {
 
     let options = Options::from(cli);
 
-    let mut releases = Releases::new();
+    let mut releases = Releases::new(&manifest);
+    std::dbg!(&releases);
     let mut updated_channels = Vec::new();
     for mut channel in manifest.get_channels() {
         println!("  - Channel: {}", channel.name);
