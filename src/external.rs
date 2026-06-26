7ef614f239683100486b45182ce428280ccd18fa
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2026-06-26 12:28:41 -0400

    install: clean up artifact download helper

--- src/external.rs
+++ src/external.rs
@@ -9,29 +9,25 @@ const HTTP_ERROR_CODES: std::ops::Range<u32> = 400..500;
 
 #[allow(dead_code)]
 pub fn install_artifact(uri: &str, to: impl AsRef<std::path::Path>) -> Result<(), String> {
+    use std::io::Write;
+
     let to = to.as_ref();
     if let Some(binary_path) = uri.strip_prefix("file://") {
-        std::fs::copy(binary_path, to).map_err(|err| {
-            format!("Failed to copy binary file to {} because of {}", to.display(), err)
-        })?;
+        std::fs::copy(binary_path, to)
+            .map_err(|err| format!("failed to copy {binary_path} -> {}: {err}", to.display()))?;
     } else if uri.starts_with("https://") {
         let mut data = Vec::new();
-        let mut handle = curl::easy::Easy::new();
-        handle
-            .follow_location(true)
-            .map_err(|_| String::from("Failed to set curl up"))?;
-        handle.url(uri).map_err(|error| {
-            format!("Error while trying to fetch binary: {}", error.description())
-        })?;
         {
-            let response_code = handle.response_code().map_err(|_| {
-                String::from(
-                    "Failed to get response code from webpage; despite HTTP protocol supporting \
-                     it.",
-                )
+            let mut handle = curl::easy::Easy::new();
+            handle.follow_location(true).map_err(|_| String::from("failed to setup curl"))?;
+            handle.url(uri).map_err(|error| {
+                format!("invalid artifact uri '{uri}': {}", error.description())
             })?;
+            let response_code = handle
+                .response_code()
+                .map_err(|err| format!("request failed for '{uri}' with unknown status: {err}"))?;
             if HTTP_ERROR_CODES.contains(&response_code) {
-                return Err(format!("Webpage returned error. Does {} exist?", uri));
+                return Err(format!("request failed for '{uri}' with status {response_code}"));
             }
 
             let mut transfer = handle.transfer();
@@ -41,34 +37,29 @@ pub fn install_artifact(uri: &str, to: impl AsRef<std::path::Path>) -> Result<()
                     Ok(new_data.len())
                 })
                 .unwrap();
-            transfer.perform().map_err(|error| {
-                format!("Error while trying to fetch binary: {}", error.description())
-            })?
+            transfer
+                .perform()
+                .map_err(|error| format!("transfer failed for '{uri}': {error}"))?
         }
         if data.is_empty() {
-            return Err(format!("Found webpage {} to be empty.", uri));
+            return Err(format!("invalid artifact: content downloaded from '{uri}' is empty"));
         }
         let tmp = to.with_extension("tmp");
         let mut file = std::fs::File::create(&tmp).map_err(|error| {
-            format!("Failed to create download file in {} because of {}", to.display(), error)
+            format!("failed to create temporary file '{}' for artifact: {error}", to.display())
         })?;
         // We set the same flags that cargo uses when producing an executable.
         file.set_permissions(
             <std::fs::Permissions as std::os::unix::fs::PermissionsExt>::from_mode(0o755),
         )
-        .map_err(|error| {
-            format!("Failed to set permissions in {} because of {}", to.display(), error)
+        .map_err(|error| format!("failed to set permissions on '{}': {error}", to.display()))?;
+        file.write_all(&data)
+            .map_err(|error| format!("failed to write artifact to '{}': {error}", to.display()))?;
+        std::fs::rename(&tmp, to).map_err(|error| {
+            format!("failed to rename {} -> {}: {error}", tmp.display(), to.display())
         })?;
-        std::io::Write::write_all(&mut file, &data).map_err(|error| {
-            format!("Failed to write download file to {} because of {}", to.display(), error)
-        })?;
-        std::fs::rename(&tmp, to)
-            .unwrap_or_else(|e| panic!("Couldn't tmp binary to {} because {}", to.display(), e));
     } else {
-        return Err(format!(
-            "Unrecognized URI type: {}. Supported URI's are 'https://' and 'file//'",
-            uri
-        ));
+        return Err(format!("unsupported uri scheme for '{uri}', must be one of: 'https', 'file'"));
     }
 
     Ok(())
